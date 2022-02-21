b02a5e34d58fc4cf5ce24cdacb06f46fee34ba0e
cryptodapp <96749852+kenjicncr@users.noreply.github.com>
2022-02-21 09:36:52 -0500

    chore; new stuff

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,17 +1,19 @@
 import { SafeTransferFromCall } from './types/NCRVDay/NCRVDayABI'
 import { VDayCard } from './types/schema'
 export function handleSafeTransferFrom(event: SafeTransferFromCall): void  {
-  let id = event.inputs.tokenId.toHex()
+  let id = event.transaction.hash.toHex()
   let tokenId = event.inputs.tokenId
+
 
   let message = event.inputs.message_
   let nickname = event.inputs.nickname_
   let sender = event.inputs.from
   let receiver = event.inputs.to
 
+
   let vdayCard = VDayCard.load(id)
   
-  if(vdayCard === null) {
+  if(!vdayCard) {
     vdayCard = new VDayCard(id)
 
     vdayCard.tokenId = tokenId
@@ -19,6 +21,7 @@ export function handleSafeTransferFrom(event: SafeTransferFromCall): void  {
     vdayCard.nickname = nickname
     vdayCard.sender = sender
     vdayCard.receiver = receiver
+    vdayCard.response = null
 
     vdayCard.save()
   }
