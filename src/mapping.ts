cf1f0a016246c28fae090bcae5da863ab3216588
cryptodapp <96749852+kenjicncr@users.noreply.github.com>
2022-02-21 14:55:28 -0500

    chore: added vdaycard response

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,12 @@
 import { log, Value } from '@graphprotocol/graph-ts'
-import { SafeTransferFromCall } from './types/NCRVDay/NCRVDayABI'
-import { VDayCard } from './types/schema'
+import { RespondCall, SafeTransferFromCall } from './types/NCRVDay/NCRVDayABI'
+import { ValentinesDayCard, ValentinesDayCardResponse } from './types/schema'
 
 export function handleSafeTransferFrom(call: SafeTransferFromCall): void  {
   let id = call.transaction.hash.toHex()
   let tokenId = call.inputs.tokenId
+
+  call.block.timestamp
 
   log.debug(`{}`, [id])
 
@@ -13,20 +15,48 @@ export function handleSafeTransferFrom(call: SafeTransferFromCall): void  {
   let sender = call.inputs.from
   let receiver = call.inputs.to
 
-  let vdayCard = VDayCard.load(id)
+  let vdayCard = ValentinesDayCard.load(id)
 
   if(!vdayCard) {
       
-    vdayCard = new VDayCard(id)
-    vdayCard.set("id", Value.fromString(id))
-    vdayCard.set("message", Value.fromString(message))
-    vdayCard.set("nickname", Value.fromString(nickname))
-    vdayCard.set("sender", Value.fromAddress(sender))
-    vdayCard.set("receiver", Value.fromAddress(receiver))
+    vdayCard = new ValentinesDayCard(id)
+    vdayCard.tokenId = tokenId
+    vdayCard.message = message
+    vdayCard.nickname = nickname
+    vdayCard.receiver = receiver
+    vdayCard.sender = sender
+
+    // create an empty response with only this sender as the receiver
+    let vdayCardResponse = new ValentinesDayCardResponse(tokenId.toHexString())
+    vdayCardResponse.receiver = sender
 
     vdayCard.save()
+    vdayCardResponse.save()
   } {
      // let vdayCard = VDayCard.load(id)
+  }
+}
+
+export function handleRespond(call: RespondCall): void {
+  let tokenId = call.inputs.tokenId
+  let id = tokenId.toHexString()
+  let message = call.inputs.message_
+  let nickname = call.inputs.nickname_
+  let state = call.inputs.state
+
+  let vdayCardResponse = ValentinesDayCardResponse.load(id)
+
+  // only create response if it doesn't already exist
+
+  if(!vdayCardResponse) {
+    vdayCardResponse = new ValentinesDayCardResponse(id)
+    vdayCardResponse.message = message
+    vdayCardResponse.nickname = nickname
+    vdayCardResponse.tokenId = tokenId
+    vdayCardResponse.sender = call.from
+    vdayCardResponse.state = state
+
+    vdayCardResponse.save()
   }
 
 }
